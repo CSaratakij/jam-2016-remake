0966e5d70876bfc9f572fea765bd8d8d53851bf1
Chatchai Saratakij <Chatchai.qrz330@gmail.com>
2016-08-23 04:17:57 +0700

    Fixed intro skiping to avoid delay when player press skip button and enabled stretch to make window shrink as the window aspect ratio (kind of ugly than manually handle camera size coresponding to screen resolution, But since we haven't get any more high resolution assets anymore -> It should be fine on the fly :) )

--- src/intro/intro.gd
+++ src/intro/intro.gd
@@ -3,6 +3,8 @@ extends Control
 
 const NEXT_SCENE_PATH = "res://menu/mainmenu.tscn"
 
+var is_force_skip = false
+
 onready var tree = get_tree()
 onready var _timer = get_node("Timer")
 onready var _video_player = get_node("VideoPlayer")
@@ -11,12 +13,15 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
-	if not _video_player.is_playing():
-		if _timer.get_time_left() == 0.0:
-			_timer.start()
+	if is_force_skip:
+		tree.change_scene(NEXT_SCENE_PATH)
+	else:
+		if not _video_player.is_playing():
+			if _timer.get_time_left() == 0.0:
+				_timer.start()
 
 func _on_btnSkip_pressed():
-	_video_player.stop()
+	is_force_skip = true
 
 func _on_Timer_timeout():
 	tree.change_scene(NEXT_SCENE_PATH)
