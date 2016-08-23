d93509311e0f0336c18e592b3ea0c723c0bc569e
Chatchai Saratakij <Chatchai.qrz330@gmail.com>
2016-08-23 18:29:15 +0700

    Added original font and added GamePause label when player pressed pause button

--- src/menu/game_menu.gd
+++ src/menu/game_menu.gd
@@ -2,10 +2,19 @@
 extends Control
 
 onready var tree = get_tree()
+onready var lblGamePause = get_node("lblGamePause")
+
+func _ready():
+	lblGamePause.hide()
 
 func _on_btnRestart_pressed():
 	if not tree.is_paused():
 		tree.reload_current_scene()
 
 func _on_btnPause_pressed():
-	tree.set_pause(not tree.is_paused())
+	if not tree.is_paused():
+		tree.set_pause(true)
+		lblGamePause.show()
+	else:
+		tree.set_pause(false)
+		lblGamePause.hide()
