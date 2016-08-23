1839d4e09e83a28b5036351cfb2d3c818a52c4d0
Chatchai Saratakij <Chatchai.qrz330@gmail.com>
2016-08-23 16:04:01 +0700

    Import sounds

--- src/menu/mainmenu.gd
+++ src/menu/mainmenu.gd
@@ -3,5 +3,7 @@ extends Control
 
 const NEXT_SCENE_PATH = "res://how_to_play/how_to_play.tscn"
 
+onready var tree = get_tree()
+
 func _on_btnPlay_pressed():
-	get_tree().change_scene(NEXT_SCENE_PATH)
+	tree.change_scene(NEXT_SCENE_PATH)
