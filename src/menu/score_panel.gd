a5cc249e842d48ae8161f56a3c753252dfcacc31
Chatchai Saratakij <Chatchai.qrz330@gmail.com>
2016-08-25 04:15:35 +0700

    Update the score of the score panel after game over.

--- src/menu/score_panel.gd
+++ src/menu/score_panel.gd
@@ -4,6 +4,7 @@ extends Node2D
 onready var tree = get_tree()
 onready var root = tree.get_root()
 onready var global = root.get_node("/root/global")
+onready var lblDynamicScore = get_node("Panel/Score/lblDynamicScore")
 
 func _ready():
 	set_process(true)
@@ -11,6 +12,8 @@ func _ready():
 
 func _process(delta):
 	if global.is_game_over():
+		var score = global.get_score()
+		lblDynamicScore.set_text(str(score))
 		show()
 	else:
 		hide()
