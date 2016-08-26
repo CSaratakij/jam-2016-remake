0e3a6e5ecd565b5b17229bc9ed2e5ae4fa87cdb3
Chatchai Saratakij <Chatchai.qrz330@gmail.com>
2016-08-26 13:54:56 +0700

    Reduce Magic number in ui life

--- src/ui/ui_life.gd
+++ src/ui/ui_life.gd
@@ -1,8 +1,18 @@
 
 extends HBoxContainer
 
+const RECT_WIDTH = 20
 const RECT_HEIGHT = 20
-const RECT_STEP_WIDTH = [0, 20, 40, 60]
+
+var rects_life = [
+		Rect2(0, 0, RECT_WIDTH * 0, RECT_HEIGHT),
+		Rect2(0, 0, RECT_WIDTH * 1, RECT_HEIGHT),
+		Rect2(0, 0, RECT_WIDTH * 2, RECT_HEIGHT),
+		Rect2(0, 0, RECT_WIDTH * 3, RECT_HEIGHT)
+	]
+
+var current_life = 0
+var current_rect_life = 0
 
 onready var tree = get_tree()
 onready var _life_sprite = get_node("Sprite")
@@ -12,4 +22,6 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
-	_life_sprite.set_region_rect(Rect2(0.0, 0.0, RECT_STEP_WIDTH[_health.get_current_life()], RECT_HEIGHT))
+	current_life = _health.get_current_life()
+	current_rect_life = rects_life[current_life]
+	_life_sprite.set_region_rect(current_rect_life)
